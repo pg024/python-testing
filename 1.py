692b61fcb255a499311f9471bac7a8fee5c167af
pg024 <50447024+pg024@users.noreply.github.com>
2019-05-09 17:45:33 +0200

    Update 1.py
    creato un goto tramite funzione + return funzione

--- 1.py
+++ 1.py
@@ -5,20 +5,23 @@ from sys import argv
 
 script, filename = argv
 
+def print_ask(f):
+ print("Do you want me to open it again?\n")
+ inp=input("Yes/No?")
+ if (inp.lower() == 'yes'):
+  print(x.read())
+  x.close()
+ elif (inp.lower() == 'no'):
+  print("nothing to see here! Bye.")
+  x.close()
+ else:
+  print("Specify Yes or No, press Ctrl + C to abort")
+  return print_ask(f)
+
+
 x = open(filename)
 
 print(x.read())
 x.close()
-
-print("Do you want me to open it again?\nYes/No")
-inp=input()
-if (inp.lower() == 'yes'):
-  x = open(filename)
-  print(x.read())
-  x.close()
-elif (inp.lower() == 'no'):
-  print("nothing to see here! Bye.")
-else:
-  print("Specify Yes or No, press Ctrl + C to abort")
-
-  
+x = open(filename)
+print_ask(x)
