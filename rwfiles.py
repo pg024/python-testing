7afafd97f29bf8ba12d7436724c7e271538900e6
pg024 <50447024+pg024@users.noreply.github.com>
2019-05-09 15:29:52 +0200

    Create rwfiles.py

---
+++ rwfiles.py
@@ -0,0 +1,11 @@
+ from sys import argv
+ 
+ script, filename = argv
+ 
+ print(f"We're going to erase {filename}")
+ print("If you don't want to, hit CTRL-C (^C)."
+ print("If you agree, hit RETURN")
+ 
+ input("?")
+ 
+ 
