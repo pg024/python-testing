17109769ff24a2d857ea064503ed248edde74b5a
pg024 <50447024+pg024@users.noreply.github.com>
2019-05-09 12:57:13 +0200

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,4 +1,13 @@
 #First python script. 
-#It reads some file
+#It reads a file and prints the content.
 
 import sys from argv
+
+filename = argv
+
+x = open(filename, w)
+print(x.read())
+x.close()
+
+print("Bye!")
+
